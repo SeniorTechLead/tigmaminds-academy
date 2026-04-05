40055c487f56ec84d378e4509b73b845c625f93d
SeniorTechLead <mintu.bora@gmail.com>
2026-04-05 20:05:02 +0530

    Register Cathedrals Levels 2-4

--- src/components/levels/index.ts
+++ src/components/levels/index.ts
@@ -175,7 +175,7 @@ const registry: Record<string, LevelComponents> = {
   'mayan-astronomy': { Level1: lazy(() => import('./MayaAstronomyLevel1')) },
   'persian-ice-houses': { Level1: lazy(() => import('./PersianIceLevel1')), Level2: lazy(() => import('./PersianIceLevel2')), Level3: lazy(() => import('./PersianIceLevel3')), Level4: lazy(() => import('./PersianIceLevel4')) },
   'zheng-he-fleet': { Level1: lazy(() => import('./ZhengHeLevel1')), Level2: lazy(() => import('./ZhengHeLevel2')), Level3: lazy(() => import('./ZhengHeLevel3')), Level4: lazy(() => import('./ZhengHeLevel4')) },
-  'gothic-cathedrals': { Level1: lazy(() => import('./CathedralsLevel1')) },
+  'gothic-cathedrals': { Level1: lazy(() => import('./CathedralsLevel1')), Level2: lazy(() => import('./CathedralsLevel2')), Level3: lazy(() => import('./CathedralsLevel3')), Level4: lazy(() => import('./CathedralsLevel4')) },
   'ibn-al-haytham-optics': { Level1: lazy(() => import('./IbnAlHaythamLevel1')), Level2: lazy(() => import('./IbnAlHaythamLevel2')), Level3: lazy(() => import('./IbnAlHaythamLevel3')), Level4: lazy(() => import('./IbnAlHaythamLevel4')) },
   'silk-road-network': { Level1: lazy(() => import('./SilkRoadLevel1')) },
   'leonardo-machines': { Level1: lazy(() => import('./LeonardoLevel1')) },
