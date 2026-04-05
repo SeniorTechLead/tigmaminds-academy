a85edc938872ac7ff1edff1a0511baf858ace970
SeniorTechLead <mintu.bora@gmail.com>
2026-04-05 19:14:13 +0530

    Register Panama Canal Levels 2-4

--- src/components/levels/index.ts
+++ src/components/levels/index.ts
@@ -156,7 +156,7 @@ const registry: Record<string, LevelComponents> = {
 
   // World History lessons
   'great-wall-of-china': { Level1: lazy(() => import('./GreatWallLevel1')), Level2: lazy(() => import('./GreatWallLevel2')), Level3: lazy(() => import('./GreatWallLevel3')), Level4: lazy(() => import('./GreatWallLevel4')) },
-  'panama-canal': { Level1: lazy(() => import('./PanamaCanalLevel1')) },
+  'panama-canal': { Level1: lazy(() => import('./PanamaCanalLevel1')), Level2: lazy(() => import('./PanamaCanalLevel2')), Level3: lazy(() => import('./PanamaCanalLevel3')), Level4: lazy(() => import('./PanamaCanalLevel4')) },
   'library-of-alexandria': { Level1: lazy(() => import('./AlexandriaLevel1')) },
   'viking-navigation': { Level1: lazy(() => import('./VikingNavLevel1')) },
   'building-the-pyramids': { Level1: lazy(() => import('./PyramidsLevel1')) },
